8c523c522d05bc12524dfd972db5de927066701d
jderaymond <jusderay@amazon.com>
2021-07-15 17:48:44 -0400

    make port dynamic

--- index.js
+++ index.js
@@ -88,6 +88,8 @@ if (dest === undefined) {
     return res.send({status : "sucessfully deleted"})
 })
 
-server.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
     console.log("Server listening")
 });
