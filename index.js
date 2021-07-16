4dddac05c70f0ad7f873ba30aee5858b615e1710
jderaymond <jusderay@amazon.com>
2021-07-16 15:38:54 -0400

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
+server.use(cors())
 server.use(express.json());
 
 const {db:destination} = require('./DB');
