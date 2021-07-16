4828d736fe2ff17da15b245d7949244c59fb2576
jderaymond <jusderay@amazon.com>
2021-07-16 15:51:50 -0400

    updated photo links

--- DB/index.js
+++ DB/index.js
@@ -3,16 +3,16 @@ const db = {
     _id: "1111",
     name: "Eiffel Tower",
     location: "Paris, France",
-    photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.thespiritsbusiness.com%2Fcontent%2Fhttp%3A%2F%2Fwww.thespiritsbusiness.com%2Fmedia%2F2020%2F07%2FParis.jpg&imgrefurl=https%3A%2F%2Fwww.thespiritsbusiness.com%2F2020%2F07%2Ftop-bars-in-paris-france%2F&tbnid=8MfFJSHZ94HiuM&vet=12ahUKEwjcnN-WpuPxAhXVgHIEHSX7DKsQMygBegUIARDNAQ..i&docid=p7xDehMFlUljiM&w=650&h=410&q=paris%20france&ved=2ahUKEwjcnN-WpuPxAhXVgHIEHSX7DKsQMygBegUIARDNAQ",
-    description: ""
+    photo: "https://images.app.goo.gl/im6HRDBrxCgQCTJJ9",
+    description: "historical building in france"
 },
 2222: 
 {
     _id: "2222",
     name: "Collosieum",
     location: "Rome, Italy",
-    photo: "",
-    description: "",
+    photo: "https://images.app.goo.gl/z2aRqRpqYTQfETiC7",
+    description: "really old",
 }
 
 };
